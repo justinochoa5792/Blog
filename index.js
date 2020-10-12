82e558a12443f1262537a9a0eccb112243b118ca
Justin Ochoa <just.och92@gmail.com>
2020-10-12 12:34:07 -0400

    blog w/ infinite scrolling

---
+++ index.js
@@ -0,0 +1,79 @@
+const postContainer = document.getElementById("post-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 5;
+let page = 1;
+
+// fetch post
+async function getPosts() {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await response.json();
+
+  return data;
+}
+
+// show posts
+async function showPosts() {
+  const post = await getPosts();
+
+  post.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    ;
+      `;
+    postContainer.appendChild(postEl);
+  });
+}
+
+// show loading
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(function () {
+    loading.classList.remove("show");
+
+    setTimeout(function () {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
+// show initial post
+showPosts();
+
+// event listener
+window.addEventListener("scroll", function () {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener("input", filterPosts);
